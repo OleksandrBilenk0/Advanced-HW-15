ebd1180e28f711cbf0cf95750611831c51ff1242
Oleksandr Bilenko <oleksandrbilenko224@gmail.com>
2023-02-14 17:41:35 +0200

    webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss'
+import andromeda from './assets/Andromeda.jpg'
+
+const mainImage = document.getElementById("mainImage")
+mainImage.src = andromeda
+
+const hello = require('./js/hello.js')
